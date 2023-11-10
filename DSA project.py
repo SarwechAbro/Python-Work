a31f8dc2311942bb7cf946784e8ce2d080b79f90
SarwechAbro <sarwechabro@gmail.com>
2023-11-10 18:57:05 +0500

    new article added

--- DSA project.py
+++ DSA project.py
@@ -4,20 +4,27 @@ import requests
 import pandas as pd
 import numpy as np
 
+
+
+url = 'https://books.sindhsalamat.com/book/87/read/2843'
+source = requests.get(url,url)
+soup = BeautifulSoup(source.text,'html.parser')
+
+for paragraph in soup.find_all('article', class_='row card article-row'):
+     article = paragraph.main.p.text
+     
+
 url = 'https://books.sindhsalamat.com/book/87/read/2838'
 source = requests.get(url)
 soup = BeautifulSoup(source.text,'html.parser')
 
 for paragraph in soup.find_all('article', class_='row card article-row'):
-     article = paragraph.main.p.text
-     mylist = article.split()
-     print(mylist)
-
-url = 'https://books.sindhsalamat.com/book/87/read/2843'
-source = requests.get(url)
-soup = BeautifulSoup(source.text,'html.parser')
-
-for paragraph in soup.find_all('article', class_='row card article-row'):
-     article = paragraph.main.p.text
-     mylist = article.split()
-     print(mylist)
+     article2 = paragraph.main.p.text
+     
+     merged_articles = article + " " + article2
+     encoded_string = merged_articles.encode('utf-16')
+     with open('python.txt', 'wb') as f:
+      f.write(encoded_string)
+      f.close()
+    
+     
